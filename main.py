1750c5eca8c10757baf7aeb46bdf8ab99c0ea7af
b3n_b3n <bencik42@gmail.com>
2021-06-03 11:05:17 +0200

    fixing bugs from new features

--- main.py
+++ main.py
@@ -28,50 +28,62 @@ relief = 'groove'
 font = [('ms sans', '13'), ('ms sans', '11'), ('ms sans', '9')]
 
 
-g = tkinter.Canvas(root, width=cw, height=ch, bg='grey80', highlightthickness=0)  # 1000x600
-g.grid(row=0, column=1, rowspan=2, sticky='s')
+g = tkinter.Canvas(root, width=cw, height=ch, bg='grey80',
+                   highlightthickness=0)  # 1000x600
+g.grid(row=1, column=1, rowspan=2, sticky='s')
+
+err_lab = tkinter.Label(root, text='tuto je balel',
+                        font=font[1], fg='red', bg=bg)
+err_lab.grid(row=0, column=1, pady=(10, 0))
 
 # sample bolt
 sp_bolt = {'name': [None],
-        'diameter[mm]': [None],
-        'x-position': [None],
-        'y-position': [None],
-        'E[MPa]': [None],
-        'Rm[MPa]': [None],
-        't[mm]': [None],
-        't2[mm]': [None]}
+           'diameter[mm]': [None],
+           'x-position': [None],
+           'y-position': [None],
+           'E[MPa]': [None],
+           'Rm[MPa]': [None],
+           't[mm]': [None],
+           't2[mm]': [None]}
 
 # sample force
 sp_force = {'name': [None],
-        'size': [None],  
-        'x-position': [None],
-        'y-position': [None],
-        'angle': [None]}
+            'size[N]': [None],
+            'x-position': [None],
+            'y-position': [None],
+            'angle[deg]': [None]}
 
 
 def calculate_centroid(bolts):
-        x = sum(bolts['x-position']) / len(bolts['x-position'])
-        y = sum(bolts['y-position']) / len(bolts['y-position'])
-        return [x,y]
+        l = len(bolts['x-position'])
+    x = sum(bolts['x-position'][1:l]) / len(bolts['x-position'])
+    y = sum(bolts['y-position'][1:l]) / len(bolts['y-position'])
+    return [x, y]
+
 
 def centroid_and_scheme(bolt, force):
+    try:
         centroid = calculate_centroid(inpt.bolt_info)
         sktch.redraw(inpt.bolt_info, inpt.force_info, centroid)
+    except:
+        err_lab.config(text='there are no geometry data')
+
 
 def create_buttons(sktch, inpt):
-        button_id = ['draw', 'calculate', 'genrate report', 'multiple reports']
-        functions = [lambda: centroid_and_scheme(inpt.bolt_info, inpt.force_info), 
-                    lambda: print(inpt.bolt_info),
-                    sktch.idk, 
-                    sktch.idk]
+    button_id = ['draw', 'calculate', 'genrate report', 'multiple reports']
+    functions = [lambda: centroid_and_scheme(inpt.bolt_info, inpt.force_info),
+                 lambda: print(inpt.bolt_info),
+                 sktch.idk,
+                 sktch.idk]
 
-        for index, id in enumerate(button_id):
-            tkinter.Button(inpt.buttons, text=id, command=functions[index], width=15,
-                        font=font[1], bg=bg, relief=relief).grid(row=index//2, 
-                        column=index % 2, sticky='e'+'w', padx=2, pady=2)
-                        
+    for index, id in enumerate(button_id):
+        tkinter.Button(inpt.buttons, text=id, command=functions[index], width=15,
+                       font=font[1], bg=bg, relief=relief).grid(row=index//2,
+                                                                column=index % 2, sticky='e'+'w', padx=2, pady=2)
+
+
 inpt = input_interface.UI(root, bg, font, sp_bolt, sp_force, dname)
-sktch = scheme.Scheme(g, inpt, cw, ch)
+sktch = scheme.Scheme(g, inpt, cw, ch, err_lab)
 create_buttons(sktch, inpt)
 
 
