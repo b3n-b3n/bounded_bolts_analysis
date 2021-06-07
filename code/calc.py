1ee12ba453fe04887af69b7cea343992cfb1e822
b3n_b3n <bencik42@gmail.com>
2021-06-07 05:15:24 +0200

    structuring the module for calculations

---
+++ code/calc.py
@@ -0,0 +1,23 @@
+import input_interface
+import scheme
+
+class Vectors:
+    def __init__(self):
+        pass
+
+
+class Calculate:
+    def __init__(self, centroid):
+        self.G_denom = 2.6  # denominator in calculation of G
+    
+    
+    def draw_resulting_vectors(self):
+        pass
+
+    def shear_load(self):
+        pass
+
+    def shear_load_moment(self):
+        pass
+
+    
