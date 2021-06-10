d8dd0cff7a29229f3fcfc618840c89268e9a849b
b3n_b3n <bencik42@gmail.com>
2021-06-10 06:27:52 +0200

    load without moemnt

--- code/calc.py
+++ code/calc.py
@@ -9,8 +9,8 @@ class Vectors:
     def convert_to_vector(self, force):
         vec = []
         for i in range(len(force['name'])):
-            x = force['size[N]'][i] / math.cos(force['angle[deg]'])
-            y = force['size[N]'][i] / math.sin(force['angle[deg]'])
+            x = force['size[N]'][i] / math.cos(force['angle[deg]'][i])
+            y = force['size[N]'][i] / math.sin(force['angle[deg]'][i])
             vec.append((x,y))
         return vec
 
@@ -24,9 +24,9 @@ class Calculate:
         self.force = inpt.force_info
         
         # resulting vectors
-        self.res_vec = {}
-        self.res_vec['load_vector'] = []
-        self.res_vec['load-moment_vector'] = []
+        self.res_vect = {}
+        self.res_vect['load_vector'] = []
+        self.res_vect['load-moment_vector'] = []
     
 
     def draw_resulting_vectors(self):
@@ -48,14 +48,23 @@ class Calculate:
                 Ai = self.bolt['diameter[mm]'][i]**2*math.pi / 4
                 finVec[0] += vect[j][0]*(Ai*Gi / sA)
                 finVec[1] = vect[j][1]*(Ai*Gi / sA)
-            self.res_vec['load_vector'].append(finVec)
+            self.res_vect['load_vector'].append(finVec)
 
 
     def shear_load_moment(self):
+        pass
+
+    def sum_resulting_vectors(self):
         pass
 
     def calc_driver(self, centroid):
         self.err_lab.config(text='')
         vect = Vectors().convert_to_vector(self.force)
 
+        self.shear_load(vect)
+        
+
+        return self.res_vect['load_vector'] 
+        # tato funkcia vrati vysledny vektor do main s čoho sa to
+        # potom prekreslí v sketchi
 
