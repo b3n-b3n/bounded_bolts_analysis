318b9f51483c7f34190bba068076651041efeb5d
b3n_b3n <bencik42@gmail.com>
2021-06-23 06:05:09 +0200

    making the class OutCalc more readable

--- code/calc.py
+++ code/calc.py
@@ -7,15 +7,19 @@ import math
 import input_interface
 import scheme
 
+
 class Auxilliary:
     """ this class is host to funcitons which serve for auxilliary
     calculation in the analysis """
+
     def convert_to_vector(self, force):
         vec = []
         for i in range(len(force['name'])):
-            x = math.cos(math.radians(force['angle[deg]'][i])) * force['force[N]'][i]
-            y = math.sin(math.radians(force['angle[deg]'][i])) * force['force[N]'][i]
-            vec.append((x,y))
+            x = math.cos(math.radians(
+                force['angle[deg]'][i])) * force['force[N]'][i]
+            y = math.sin(math.radians(
+                force['angle[deg]'][i])) * force['force[N]'][i]
+            vec.append((x, y))
         return vec
 
     def distance_from_centroid(self, cx, cy, x, y):
@@ -41,21 +45,22 @@ class Auxilliary:
 
     def invert_vector(self, vect):
         for i in range(len(vect)):
-            vect[i][0] *= -1 
+            vect[i][0] *= -1
             vect[i][1] *= -1
         return vect
+
 
 class Calculate:
     def __init__(self, err_lab, inpt):
         self.G_denom = 2.6  # denominator in calculation of G
         self.err_lab = err_lab
-        
+
         # data provided by user
         self.bolt = inpt.bolt_info
         self.force = inpt.force_info
 
         self.aux = Auxilliary()
-        
+
         # outputs of the data
         self.shear_load = []
         self.moment_load = []
@@ -73,7 +78,7 @@ class Calculate:
         # final vector after adding all forces
         # for every bolt considers every force
         for i in range(len(self.bolt['name'])):
-            finVec = [0,0]
+            finVec = [0, 0]
             for j in range(len(vect)):
                 Gi = self.bolt['E[MPa]'][i]/2.6
                 Ai = self.bolt['diameter[mm]'][i]**2*math.pi / 4
@@ -81,7 +86,6 @@ class Calculate:
                 finVec[1] += vect[j][1]*(Ai*Gi / sA)
             out.append(finVec)
         return out
-
 
     def moment_load_func(self, c, vect, moment_of_force):
         sA = 0
@@ -91,13 +95,13 @@ class Calculate:
             Gi = self.bolt['E[MPa]'][i]/2.6
             x, y = self.bolt['x-pos[mm]'][i], self.bolt['y-pos[mm]'][i]
             d = self.aux.distance_from_centroid(c[0], c[1], x, y)
-            sA += Ai* Gi * d**2    
-        
+            sA += Ai * Gi * d**2
+
         M = self.aux.force_moment(c, self.force, vect)
         M += moment_of_force
 
         for i in range(len(self.bolt['name'])):
-            finVec = [0,0]
+            finVec = [0, 0]
             for j in range(len(vect)):
                 Gi = self.bolt['E[MPa]'][i]/2.6
                 Ai = self.bolt['diameter[mm]'][i]**2*math.pi / 4
@@ -109,7 +113,6 @@ class Calculate:
                 # this here needs to be done
             out.append(finVec)
         return out
-           
 
     def sum_resulting_vectors(self):
         pass
@@ -120,31 +123,36 @@ class Calculate:
         calc_out = {}
 
         self.shear_load = self.aux.invert_vector(self.shear_load_func(vect))
-        self.moment_load = self.aux.invert_vector(self.moment_load_func(centroid, vect, force_moment))
+        self.moment_load = self.aux.invert_vector(
+            self.moment_load_func(centroid, vect, force_moment))
         self.sum_load = self.aux.zip_vectors(self.shear_load, self.moment_load)
 
 
 class OutCalc:
+    """ 
+    method create_dataframe returns the dicionary needed for the output table
+    other methods are used for evaluation of the results and auxiliary calculations
+    """
+
     def __init__(self, calc, inpt):
         self.inpt = inpt
         self.calc = calc
         self.bolts_num = len(self.inpt.bolt_info['name'])
         self.round_to = 2
 
-
     def calculate_tau(self, vect):
         out = []
-        if vect[0] == '-': return ['-' for i in range(self.bolts_num)]
-        
+        if vect[0] == '-':
+            return ['-' for i in range(self.bolts_num)]
         d = self.inpt.bolt_info['diameter[mm]']
         for i in range(len(vect)):
             area = math.pi*d[i] / 4
             out.append(round(float(vect[i])*area, self.round_to))
         return out
 
-
     def calculate_sigma(self, vect, t):
-        if vect[0] == '-': return ['-' for i in range(self.bolts_num)]
+        if vect[0] == '-':
+            return ['-' for i in range(self.bolts_num)]
         out = []
         d = self.inpt.bolt_info['diameter[mm]']
         for i in range(len(vect)):
@@ -152,27 +160,28 @@ class OutCalc:
         return out
 
     def calculate_rfi(self, sigma, Fbry):
-        if sigma[0] == '-': return ['-' for i in range(self.bolts_num)]
+        if sigma[0] == '-':
+            return ['-' for i in range(self.bolts_num)]
         out = []
         print(Fbry)
         for sig in sigma:
             out.append(round(Fbry/sig, self.round_to))
         return out
 
-
     def calculate_rf(self, tau):
-        if tau[0] == '-': return ['-' for i in range(self.bolts_num)]
+        if tau[0] == '-':
+            return ['-' for i in range(self.bolts_num)]
         out = []
         rm = self.inpt.bolt_info['Rm[MPa]']
-        for i in range(self.bolts_num): 
+        for i in range(self.bolts_num):
             out.append(round(rm[i]/2/tau[i], self.round_to))
         return out
 
     def vect_to_size(self, vect):
         # takes vector and calculates its size
-        if not vect: return ['-' for i in range(self.bolts_num)]
-        return [round(math.sqrt(vect[i][0]**2 + vect[i][1]**2), self.round_to) for i in range(len(vect))] 
-
+        if not vect:
+            return ['-' for i in range(self.bolts_num)]
+        return [round(math.sqrt(vect[i][0]**2 + vect[i][1]**2), self.round_to) for i in range(len(vect))]
 
     def create_dataframe(self):
         tab_data = {}  # table data
@@ -183,11 +192,14 @@ class OutCalc:
         tab_data['F [N]'] = self.vect_to_size(self.calc.sum_load)
         tab_data['τ [MPa]'] = self.calculate_tau(tab_data['F [N]'])
         tab_data['RF-0 [MPa]'] = self.calculate_rf(tab_data['τ [MPa]'])
-        tab_data['σ1 [MPa]'] = self.calculate_sigma(tab_data['F [N]'], self.inpt.bolt_info['t1[mm]'])
-        tab_data['σ2 [MPa]'] = self.calculate_sigma(tab_data['F [N]'], self.inpt.bolt_info['t2[mm]'])
-        tab_data['RF-1'] = self.calculate_rfi(tab_data['σ1 [MPa]'], float(self.inpt.object1['Fbry[MPa]'].get()))
-        tab_data['RF-2'] = self.calculate_rfi(tab_data['σ2 [MPa]'], float(self.inpt.object2['Fbry[MPa]'].get()))
+        tab_data['σ1 [MPa]'] = self.calculate_sigma(
+            tab_data['F [N]'], self.inpt.bolt_info['t1[mm]'])
+        tab_data['σ2 [MPa]'] = self.calculate_sigma(
+            tab_data['F [N]'], self.inpt.bolt_info['t2[mm]'])
+        tab_data['RF-1 [MPa]'] = self.calculate_rfi(
+            tab_data['σ1 [MPa]'], float(self.inpt.object1['Fbry[MPa]'].get()))
+        tab_data['RF-2 [MPa]'] = self.calculate_rfi(
+            tab_data['σ2 [MPa]'], float(self.inpt.object2['Fbry[MPa]'].get()))
 
         df = pandas.DataFrame(data=tab_data)
-        print(df)
         return df
