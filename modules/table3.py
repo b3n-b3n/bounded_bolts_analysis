8f8466bc645aef64fd2113611f9380d9e336d3a5
b3n_b3n <bencik42@gmail.com>
2021-06-30 13:01:11 +0200

    clening and commenting of the code

--- modules/table3.py
+++ modules/table3.py
@@ -65,82 +65,76 @@ dataframe = pandas.DataFrame({
     }
 )
 
-columns = dataframe.columns
+tables = {'fontsize': [7, 7, 7, 6], 'cols': [1, 1 , 2, len(dataframe.columns)]}
+# get the data from dataframe
 n_rows = len(dataframe)
+cell_text = [[dataframe[col][row] for col in dataframe.columns] for row in range(n_rows)]
 
-# Plot bars and create text labels for the table
-cell_text = []
-for row in range(n_rows):
-    cell_text.append([dataframe[col][row] for col in columns])
-
+# define dimensions of the plot
+# these should not be changed because cropping of image depends
+# on concrete number of pixels in the image
 f, ax = plt.subplots(figsize=(6.5, 10))
 
-# Add headers and a table at the bottom of the axes
+# create table just to get size of a singe cell for calculation
+# of position of the headers
+the_table = plt.table(cellText=cell_text, colLabels=dataframe.columns, loc='center')
 
-the_table = plt.table(cellText=cell_text,
-                     colLabels=columns,
-                     cellLoc='center',
-                     colLoc='center',
-                     loc='center')
-
+# height of the cell
 h = the_table.get_celld()[(0, 0)].get_height()
-
+# ending height of the table where headers would start
 end_h = 0.5+h*(n_rows+1)/2
 
-header_0 = plt.table(cellText=[['']],
+
+# creating the main table alongside with the headers
+tables['header_0'] = plt.table(cellText=[['']],
                     colLabels=['Bearing Strength\nAnalysis'],
                     fontsize=5,
                     loc='bottom',
                     bbox=[1 / 11 * 5, end_h-2*h, 1 / 11 * 2, h*4])
 
-header_1 = plt.table(cellText=[['']],
+tables['header_1'] = plt.table(cellText=[['']],
                     colLabels=['Bolt Stress Analysis'],
                     loc='bottom',
                     bbox=[1 / 11 * 7, end_h, 1 / 11 * 4, h*2])
 
-
-materials = plt.table(cellText=[[''] * 2],
+tables['materials'] = plt.table(cellText=[[''] * 2],
                      colLabels=['Material 1', 'Material 2'],
                      loc='bottom',
                      bbox=[1 / 11 * 7, end_h-h, 1 / 11 * 4, h*2])
 
-the_table = plt.table(cellText=cell_text,
-                     colLabels=columns,
+tables['the_table'] = plt.table(cellText=cell_text,
+                     colLabels=dataframe.columns,
                      cellLoc='center',
                      colLoc='center',
                      loc='center')
 
-header_0.auto_set_font_size(False)
-header_0.set_fontsize(8)
-header_1.auto_set_font_size(False)
-header_1.set_fontsize(8)
-materials.auto_set_font_size(False)
-materials.set_fontsize(8)
-the_table.auto_set_font_size(False)
-the_table.set_fontsize(6)
-
-for cell in range(len(columns)):
-    the_table[(0, cell)].set_text_props(fontweight='bold')
+# set font properies for the headers and column names
+for idx, key in enumerate(list(tables.keys())[2:]):
+    tables[key].auto_set_font_size(False)
+    tables[key].set_fontsize(tables['fontsize'][idx])
+    for cell in range(tables['cols'][idx]):
+        tables[key][(0, cell)].set_text_props(fontweight='bold')
 
 
-# Adjust layout to make room for the table:
+# Adjust layout of the figure
 plt.axis('off')
 plt.tight_layout(h_pad=1, w_pad=1)
+
+# transform the figure into list of pixels
 f.canvas.draw()
 buf = f.canvas.tostring_rgb()
 ncols, nrows = f.canvas.get_width_height()
 img = numpy.frombuffer(buf, dtype=numpy.uint8).reshape(nrows, ncols, 3)
 
-
-start, end = 0, nrows
-table_found = False
+# cut the image to the desired size
+start, table_found = 0, False
 for y in range(len(img)):
     if (False in (img[y][635] == 255)) and not table_found:
         table_found = True
         start = y
     if (True in (img[y][635] == 255)) and table_found:
-        end = y
+        img = img[start-15:y+15]
         break
-img = img[start-15:end+15]
+    
 pil_img = PIL.Image.fromarray(img)
 pil_img.show()
