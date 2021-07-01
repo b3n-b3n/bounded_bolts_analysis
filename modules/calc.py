702507d7ee8e11517b01964f4aa99b9861422f6b
b3n_b3n <bencik42@gmail.com>
2021-07-01 18:45:22 +0200

    typing of the functions

--- modules/calc.py
+++ modules/calc.py
@@ -1,18 +1,14 @@
 # importing libraries
+import tkinter
 import pandas
 import numpy
 import math
-
-# importing local files
-import input_interface
-import scheme
-
 
 class Auxilliary:
     """ this class is host to funcitons which serve for auxilliary
     calculation in the analysis """
 
-    def convert_to_vector(self, force):
+    def convert_to_vector(self, force: list) -> list:
         vec = []
         for i in range(len(force['name'])):
             x = math.cos(math.radians(
@@ -22,17 +18,17 @@ class Auxilliary:
             vec.append((x, y))
         return vec
 
-    def distance_from_centroid(self, cx, cy, x, y):
+    def distance_from_centroid(self, cx: int, cy: int, x: int, y: int) -> int:
         dx, dy = abs(cx - x), abs(cy - y)
         return math.sqrt(dx**2 + dy**2)
 
-    def zip_vectors(self, v1, v2):
+    def zip_vectors(self, v1, v2) -> list:
         out = []
         for i in range(len(v1)):
             out.append([v1[i][0]+v2[i][0], v1[i][1]+v2[i][1]])
         return out
 
-    def force_moment(self, c, force, vect):
+    def force_moment(self, c: int, force: list, vect: list) -> int:
         finMoment = 0
         xpos = force['x-pos[mm]']
         ypos = force['y-pos[mm]']
@@ -43,7 +39,7 @@ class Auxilliary:
             finMoment += numpy.cross(ri, fi)
         return finMoment
 
-    def invert_vector(self, vect):
+    def invert_vector(self, vect: list) -> list:
         for i in range(len(vect)):
             vect[i][0] *= -1
             vect[i][1] *= -1
@@ -51,7 +47,7 @@ class Auxilliary:
 
 
 class Calculate:
-    def __init__(self, err_lab, inpt):
+    def __init__(self, err_lab: tkinter.Label, inpt) -> None:
         self.G_denom = 2.6  # denominator in calculation of G
         self.err_lab = err_lab
 
@@ -66,7 +62,7 @@ class Calculate:
         self.moment_load = []
         self.sum_load = []
 
-    def shear_load_func(self, vect):
+    def shear_load_func(self, vect: list) -> list:
         # calulate sum of all fastener ares
         sA = 0
         out = []
@@ -87,7 +83,7 @@ class Calculate:
             out.append(finVec)
         return out
 
-    def moment_load_func(self, c, vect, moment_of_force):
+    def moment_load_func(self, c: int, vect: list, moment_of_force: int) -> list:
         sA = 0
         out = []
         for i in range(len(self.bolt['name'])):
@@ -114,30 +110,27 @@ class Calculate:
             out.append(finVec)
         return out
 
-    def sum_resulting_vectors(self):
-        pass
-
-    def calc_driver(self, centroid, force_moment):
+    def calc_driver(self, centroid: list, force_moment: int) -> None:
         self.err_lab.config(text='')
         vect = self.aux.convert_to_vector(self.force)
-        calc_out = {}
 
         self.shear_load = self.aux.invert_vector(self.shear_load_func(vect))
         self.moment_load = self.aux.invert_vector(
             self.moment_load_func(centroid, vect, force_moment))
         self.sum_load = self.aux.zip_vectors(self.shear_load, self.moment_load)
+
 class OutCalc:
     """ 
     method create_dataframe returns the dicionary needed for the output table
     other methods are used for evaluation of the results and auxiliary calculations
     """
 
-    def __init__(self, calc, inpt):
+    def __init__(self, calc, inpt) -> None:
         self.inpt = inpt
         self.calc = calc
         self.bolts_num = len(self.inpt.bolt_info['name'])
 
-    def calculate_tau(self, vect):
+    def calculate_tau(self, vect) -> list:
         out = []
         if vect[0] == '-':
             return ['-' for i in range(self.bolts_num)]
@@ -147,7 +140,7 @@ class OutCalc:
             out.append(float(vect[i])*area)
         return out
 
-    def calculate_sigma(self, vect, t):
+    def calculate_sigma(self, vect, t) -> list:
         if vect[0] == '-':
             return ['-' for i in range(self.bolts_num)]
         out = []
@@ -156,7 +149,7 @@ class OutCalc:
             out.append(vect[i] / (d[i]*t[i]))
         return out
 
-    def calculate_rfi(self, sigma, Fbry):
+    def calculate_rfi(self, sigma, Fbry) -> list:
         if sigma[0] == '-':
             return ['-' for i in range(self.bolts_num)]
         out = []
@@ -164,7 +157,7 @@ class OutCalc:
             out.append(Fbry/sig)
         return out
 
-    def calculate_rf(self, tau):
+    def calculate_rf(self, tau) -> list:
         if tau[0] == '-':
             return ['-' for i in range(self.bolts_num)]
         out = []
@@ -173,13 +166,13 @@ class OutCalc:
             out.append(rms[i]/tau[i])
         return out
 
-    def vect_to_size(self, vect):
+    def vect_to_size(self, vect) -> list:
         # takes vector and calculates its size
         if not vect:
             return ['-' for i in range(self.bolts_num)]
         return [math.sqrt(vect[i][0]**2 + vect[i][1]**2) for i in range(len(vect))]
 
-    def create_dataframe(self):
+    def create_dataframe(self) -> pandas.DataFrame:
         tab_data = {}  # table data
         tab_data['ID Number'] = self.inpt.bolt_info['name']
         tab_data['d [mm]'] = self.inpt.bolt_info['diameter[mm]']
